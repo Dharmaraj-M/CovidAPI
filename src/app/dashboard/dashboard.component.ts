64f07728bd25e9a971f3bc9ffbbf14d78a38032b
Dharmarajcs17 <dharmaraj.cs17@bitsathy.ac.in>
2021-05-17 12:27:03 +0530

    update in dashboard component and charts.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -52,10 +52,10 @@ export class DashboardComponent implements OnInit {
 	countryDeaths: number;
 	countryRecovered: number;
 
-	countryMonthlyActive: number[] = [];
-	countryMonthlyConfirmed: number[] = [];
-	countryMonthlyDeaths: number[] = [];
-	countryMonthlyRecovered: number[] = [];
+	countryMonthlyActive: number[];
+	countryMonthlyConfirmed: number[];
+	countryMonthlyDeaths: number[];
+	countryMonthlyRecovered: number[];
 	labels: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
 	ngOnInit(): void {
@@ -84,7 +84,7 @@ export class DashboardComponent implements OnInit {
 
 	getCountryData() {
 		this.api.getCountryData(this.selectedCountry).subscribe((data) => {
-			var lastValue = data.length - 1;
+			let lastValue = data.length - 1;
 			this.countryActive = data[lastValue].Active;
 			this.countryConfirmed = data[lastValue].Confirmed;
 			this.countryDeaths = data[lastValue].Deaths;
@@ -95,87 +95,78 @@ export class DashboardComponent implements OnInit {
 	getData() {
 		this.getCountryData();
 		// console.log(this.selectedCountry);
-		this.getDailyData();
 		if (this.confirmed || this.activeRecoveredDeath) {
 			this.confirmed.destroy();
 			this.activeRecoveredDeath.destroy();
 		}
-		this.createChartForConfirmed();
-		this.createChartForActiveRecoveredDeath();
+		this.getDailyData();
 	}
 
+
 	getDailyData() {
+		Chart.register(
+			ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController,
+			LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale,
+			LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Filler, Legend,
+			Title, Tooltip
+		);
 		this.api.getDailyData(this.selectedCountry).subscribe((data) => {
-			var index = data.length - 1;
-			for (var i = index; i > index - 30; i--) {
+			this.countryMonthlyActive = [];
+			this.countryMonthlyConfirmed = [];
+			this.countryMonthlyDeaths = [];
+			this.countryMonthlyRecovered = [];
+			let index = data.length - 1;
+			for (let i = index; i > index - 30; i--) {
 				this.countryMonthlyActive.push(data[i].Active);
 				this.countryMonthlyConfirmed.push(data[i].Confirmed)
 				this.countryMonthlyDeaths.push(data[i].Deaths)
 				this.countryMonthlyRecovered.push(data[i].Recovered)
 			}
-		})
-	}
-
-	createChartForActiveRecoveredDeath() {
-		Chart.register(
-			ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController,
-			LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale,
-			LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Filler, Legend,
-			Title, Tooltip
-		);
-
-		this.activeRecoveredDeath = new Chart("activeRecoveredDeath", {
-			type: 'line',
-			data: {
-				labels: this.labels,
-				datasets: [
-					{
-						label: 'Active Cases',
-						data: this.countryMonthlyActive,
-						fill: false,
-						borderColor: 'blue',
-						tension: 0.1
-					},
-					{
-						label: 'Deaths',
-						data: this.countryMonthlyDeaths,
-						fill: false,
-						borderColor: 'red',
-						tension: 0.1
-					},
-					{
-						label: 'Recovered Cases',
-						data: this.countryMonthlyRecovered,
-						fill: false,
-						borderColor: 'green',
-						tension: 0.1
-					}
-				]
-			}
-		});
-	}
-	createChartForConfirmed() {
-		Chart.register(
-			ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController,
-			LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale,
-			LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Filler, Legend,
-			Title, Tooltip
-		);
-
-		this.confirmed = new Chart("confirmed", {
-			type: 'line',
-			data: {
-				labels: this.labels,
-				datasets: [
-					{
-						label: 'Confirmed Cases',
-						data: this.countryMonthlyConfirmed,
-						fill: false,
-						borderColor: 'blueviolet',
-						tension: 0.1
-					}
-				]
-			}
+			// console.log(this.countryMonthlyConfirmed);
+			this.confirmed = new Chart("confirmed", {
+				type: 'line',
+				data: {
+					labels: this.labels,
+					datasets: [
+						{
+							label: 'Confirmed Cases',
+							data: this.countryMonthlyConfirmed,
+							fill: false,
+							borderColor: 'blueviolet',
+							tension: 0.1
+						}
+					]
+				}
+			});
+			this.activeRecoveredDeath = new Chart("activeRecoveredDeath", {
+				type: 'line',
+				data: {
+					labels: this.labels,
+					datasets: [
+						{
+							label: 'Active Cases',
+							data: this.countryMonthlyActive,
+							fill: false,
+							borderColor: 'blue',
+							tension: 0.1
+						},
+						{
+							label: 'Deaths',
+							data: this.countryMonthlyDeaths,
+							fill: false,
+							borderColor: 'red',
+							tension: 0.1
+						},
+						{
+							label: 'Recovered Cases',
+							data: this.countryMonthlyRecovered,
+							fill: false,
+							borderColor: 'green',
+							tension: 0.1
+						}
+					]
+				}
+			});
 		})
 	}
 }
